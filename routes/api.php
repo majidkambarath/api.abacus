3b4cddf378b54e1756bd3b6714dd9f3514d1d571
ABDUL MAJID K <majidmaji3410@gmail.com>
2024-06-29 12:19:53 +0530

    api route change

--- routes/api.php
+++ routes/api.php
@@ -3,40 +3,33 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\PublicController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    $user = $request->user();
+    try {
+        $user = $request->user();
+        $user->load('roles');
 
-    // Eager load the roles relationship
-    $user->load('roles');
+        $return = [
+            "name" => $user->name,
+            "id" => $user->id,
+            "role" => $user->roles[0]->name ?? 'No Role'
+        ];
 
+        if ($user->hasRole('manager')) {
+            $user->load('managedBranch');
+            $return['branch'] = $user->managedBranch ? $user->managedBranch->name : 'No Branch';
+        }
 
-    $return = [
-        "name" => $user->name,
-        "id" => $user->id,
-        "role" => $user->roles[0]["name"]
-    ];
+        if ($user->hasRole('executive')) {
+            $return['branch'] = $user->branches[0]->name ?? 'No Branch';
+        }
 
-    if($user->hasRole('manager')){
-        $user->load('managedBranch');
-        $return['branch'] = $user->managedBranch?->name;
+        return response()->json($return);
+    } catch (\Exception $e) {
+        \Log::error('Error fetching user data: ' . $e->getMessage());
+        return response()->json(['error' => 'Server error'], 500);
     }
-    if($user->hasRole('executive')){
-        $return['branch'] = $user->branches[0]->name;
-    }
-
-    return $return;
 });
 
 include __DIR__ . '/admin.php';
@@ -52,9 +45,9 @@ Route::middleware(['auth:sanctum', 'role:executive,manager,admin'])->group(funct
     Route::get('/dashboard', [DashboardController::class, 'index']);
     Route::post('/change_password', [DashboardController::class, 'changePassword']);
 });
+
 Route::get('/csrf-token', function () {
     return response()->json(['csrf_token' => csrf_token()]);
 });
 
-Route::get('/update-photos', [\App\Http\Controllers\PublicController::class, 'updatePhotos']);
-//Route::get('/delete-control', [\App\Http\Controllers\PublicController::class, 'deleteControllerFolder']);
+Route::get('/update-photos', [PublicController::class, 'updatePhotos']);
